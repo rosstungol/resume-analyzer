42e0fbbe67f6929fe3f921cb88d730cf06736567
Ross Tungol <rossjstungol@gmail.com>
2026-04-17 21:13:55 +0800

    updates fonts

--- app/root.tsx
+++ app/root.tsx
@@ -14,19 +14,6 @@ import { useEffect } from 'react'
 import { PageWrapper } from '@/components/layout/PageWrapper'
 import { usePuterStore } from '@/lib/puter'
 
-export const links: Route.LinksFunction = () => [
-  { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-  {
-    rel: 'preconnect',
-    href: 'https://fonts.gstatic.com',
-    crossOrigin: 'anonymous',
-  },
-  {
-    rel: 'stylesheet',
-    href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
-  },
-]
-
 export function Layout({ children }: { children: React.ReactNode }) {
   const { init } = usePuterStore()
 
