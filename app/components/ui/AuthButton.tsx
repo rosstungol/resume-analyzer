4501a2c05eda96d21c9df7598a9d2257e6a3c379
Ross Tungol <rossjstungol@gmail.com>
2026-05-01 01:10:59 +0800

    updates auth button logic

--- app/components/ui/AuthButton.tsx
+++ app/components/ui/AuthButton.tsx
@@ -1,21 +1,27 @@
 import { LogIn, LogOut } from 'lucide-react'
+import { useShallow } from 'zustand/shallow'
 
 import { usePuterStore } from '@/lib/puter'
 import { Button } from './Button'
 
 export function AuthButton() {
-	const auth = usePuterStore((state) => state.auth)
+	const { auth, isLoading } = usePuterStore(
+		useShallow((state) => ({
+			auth: state.auth,
+			isLoading: state.isLoading,
+		}))
+	)
 
 	if (auth.isAuthenticated)
 		return (
-			<Button variant='secondary' onClick={auth.signOut}>
+			<Button variant='secondary' onClick={auth.signOut} disabled={isLoading}>
 				<LogOut />
 				<span>Log Out</span>
 			</Button>
 		)
 
 	return (
-		<Button variant='primary' onClick={auth.signIn}>
+		<Button variant='primary' onClick={auth.signIn} disabled={isLoading}>
 			<LogIn />
 			<span>Log In</span>
 		</Button>
