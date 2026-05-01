b8622b1e6ceead03c179f55fe992beb84fee3635
Ross Tungol <rossjstungol@gmail.com>
2026-05-02 05:08:35 +0800

    updates navbar styles

--- app/components/layout/Navbar.tsx
+++ app/components/layout/Navbar.tsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router'
 
 export function Navbar({ children }: { children?: ReactNode }) {
 	return (
-		<nav className='card card-shadow flex h-fit w-full flex-col items-center justify-between gap-4 py-3 md:min-h-[88px] md:flex-row md:px-5'>
+		<nav className='card card-shadow flex h-fit min-h-18 w-full flex-col items-center justify-between gap-4 py-2 md:flex-row md:px-5'>
 			<Link to='/'>
-				<h1 className='font-black font-heading text-4xl text-accent-foreground text-stroke [text-shadow:3px_3px_0px_rgba(70,57,71,1)] lg:text-5xl'>
+				<h1 className='font-black font-heading text-4xl text-accent-foreground text-stroke [text-shadow:3px_3px_0px_rgba(70,57,71,1)]'>
 					resmyze
 				</h1>
 			</Link>
